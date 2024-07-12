ceefd566bc9033c8497f34a0c5773cc9f4db8cb7
vershinin <vershinin@banki.ru>
2024-07-12 17:05:24 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,142 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [id, setId] = useState(1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='red'>
+        <div className='orange'>
+          <h1 className='yellow'>
+            Выбирайте лучшее
+          </h1>
+          <div className='yellow2'>
+            <button className={id === 1 ? `active green` : 'green'} onClick={() => setId(1)}>
+              Для всех
+            </button>
+            <button className={id === 2 ? 'active green' : `green`} onClick={() => setId(2)}>
+              Малому бизнесу
+            </button>
+            <button className={id === 3 ? 'active green' : `green`} onClick={() => setId(3)}>
+              Крупному изнесу
+            </button>
+            
+          </div>
+        </div>
+
+        {id === 1 && (
+        <div className='blue'>
+          <div className='violet1'>
+            <h2 className='sky-title'>
+              Дебетовые карты
+            </h2>
+            <p className='sky-dicription'>
+              Суперкешбек
+            </p>
+            
+            <img className='sky-image' src='https://alfabank.servicecdn.ru/site-upload/25/51/1449/D_CardPromo_267_298_vertical_dc.png' />
+
+          </div>
+          <div className='violet1'>
+            <h2 className='sky-title'>
+              Кредитные карты
+            </h2>
+            <p className='sky-dicription'>
+              Кешбек за покупки
+            </p>
+            <img className='sky-image' src='https://alfabank.servicecdn.ru/site-upload/91/be/1449/D_CardPromo_267x298_230524.png'/>
+          </div>
+          <div className='violet1'>
+            <h2 className='sky-title'>
+              Накопления
+            </h2>
+            <p className='sky-dicription'>
+              Доход спервого месяца
+            </p>
+            <img className='sky-image' src='https://alfabank.servicecdn.ru/site-upload/e3/cf/2330/CardPromo_267x298_121123-2.png'/>
+          </div>
+          <div className='violet1'>
+            <h2 className='sky-title'>
+              Накопления
+            </h2>
+            <p className='sky-dicription'>
+              Доход спервого месяца
+            </p>
+            <img className='sky-image' src='https://alfabank.servicecdn.ru/site-upload/e3/cf/2330/CardPromo_267x298_121123-2.png'/>
+          </div>
+          <div className='violet1'>
+            <h2 className='sky-title'>
+              Накопления
+            </h2>
+            <p className='sky-dicription'>
+              Доход спервого месяца
+            </p>
+            <img className='sky-image' src='https://alfabank.servicecdn.ru/site-upload/e3/cf/2330/CardPromo_267x298_121123-2.png'/>
+          </div>
+          <div className='violet1'>
+            <h2 className='sky-title'>
+              Накопления
+            </h2>
+            <p className='sky-dicription'>
+              Доход спервого месяца
+            </p>
+            <img className='sky-image' src='https://alfabank.servicecdn.ru/site-upload/e3/cf/2330/CardPromo_267x298_121123-2.png'/>
+          </div>
+          <div className='violet1'>
+            <h2 className='sky-title'>
+              Накопления
+            </h2>
+            <p className='sky-dicription'>
+              Доход спервого месяца
+            </p>
+            <img className='sky-image' src='https://alfabank.servicecdn.ru/site-upload/e3/cf/2330/CardPromo_267x298_121123-2.png'/>
+          </div>
+        </div>
+        )}
+
+        {id === 2 && (
+          <div className='blue'>
+          <div className='violet1'>
+            <h2 className='sky-title'>
+              Накопления
+            </h2>
+            <p className='sky-dicription'>
+              Доход спервого месяца
+            </p>
+            <img className='sky-image' src='https://alfabank.servicecdn.ru/site-upload/e3/cf/2330/CardPromo_267x298_121123-2.png'/>
+          </div>
+          <div className='violet1'>
+            <h2 className='sky-title'>
+              Накопления
+            </h2>
+            <p className='sky-dicription'>
+              Доход спервого месяца
+            </p>
+            <img className='sky-image' src='https://alfabank.servicecdn.ru/site-upload/e3/cf/2330/CardPromo_267x298_121123-2.png'/>
+          </div>
+        </div>
+        )}
+        
+
+        {id === 3 && (
+          
+                    <div className='blue'>
+                      <div className='violet1'>
+                    <h2 className='sky-title'>
+                      Накопления
+                    </h2>
+                    <p className='sky-dicription'>
+                      Доход спервого месяца
+                    </p>
+                    <img className='sky-image' src='https://alfabank.servicecdn.ru/site-upload/e3/cf/2330/CardPromo_267x298_121123-2.png'/>
+                  </div>
+                  </div>
+        )}
+      </div>
     </div>
-  );
+
+  )  ;
 }
 
 export default App;
